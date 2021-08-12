5fdf52e47fce2eb0f3277341a9bc19f6de7009df
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2021-08-12 18:36:25 -0500

    add test_formatting.c - empty but for notes

---
+++ test/unit/sunlark/test_formatting.c
@@ -0,0 +1,18 @@
+
+
+/*
+
+  cases:
+
+  * prepend target
+  * append target
+  * splice one interior target, at and after
+  * splice list/vec of targets
+
+  * ditto for bindings
+
+  * ditto for vectors (Starlark lists)
+
+  * interleaved targets & load stmts
+
+ */
